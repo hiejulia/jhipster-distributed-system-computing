3f332212bc6ff65c06ce64b6beb7b54d6a2de836
hien.nguyen <hien.nguyen@elisa.fi>
2020-03-08 16:02:48 +0200

    init cache server

--- README.md
+++ README.md
@@ -5,11 +5,15 @@
 
 ## Implementation 
 + Distributed Cache server 
+    + HazelCast distributed caching 
 + Distributed messaging 
-+ Distributed DB 
+    + ActiveMQ
++ Distributed DB
+    + Cassandra  
 + Distributed file system : Hadoop, HDFS
 + Distributed DNS 
 + Distributed web server 
 + Utilize cloud services: 
     + AWS 
-+ 
+
+
---
+++ cacheserver/src/main/java/com/project/cacheserver/CacheserverApplication.java
@@ -0,0 +1,13 @@
+package com.project.cacheserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheserverApplication.class, args);
+	}
+
+}
